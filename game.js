71aa3fb54495bb6e0001a51b514334bbb4a78bd7
batisha-haab <140179586+batisha-haab@users.noreply.github.com>
2026-01-22 23:09:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,9 @@
-const tasks = [
-"7 × 8 = ?",
-"15 + 27 = ?",
-"Tap fast!",
-"9 × 6 = ?",
-"Memory test"
-];
-
-
-const grid = document.getElementById("grid");
-
-
-for (let i = 0; i < 9; i++) {
-const btn = document.createElement("button");
-btn.innerText = tasks[i % tasks.length];
-btn.onclick = () => {
-const ans = prompt(btn.innerText);
-if (ans) {
-btn.disabled = true;
-btn.style.background = "green";
+function winGame() {
+    Telegram.WebApp.sendData("WIN");
+    Telegram.WebApp.close();
 }
-};
-grid.appendChild(btn);
+
+function loseGame() {
+    Telegram.WebApp.sendData("LOSE");
+    Telegram.WebApp.close();
 }
